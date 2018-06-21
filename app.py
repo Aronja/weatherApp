17344531916d24532860052375100317beea4055
Aronja <anovicky@thoughtworks.com>
2018-06-21 18:01:19 +0100

    [AN] Refactor app, remove dublication

--- app.py
+++ app.py
@@ -6,16 +6,13 @@ user_input = raw_input("Enter a location: ")
 
 geolocator = Nominatim()
 location = geolocator.geocode(user_input)
-longitude = str(location.longitude)
-latitude = str(location.latitude)
-longitude = str(longitude).decode('utf8')
-latitude = str(latitude).decode('utf8')
+longitude = str(location.longitude).decode('utf8')
+latitude = str(location.latitude).decode('utf8')
 
 DARK_SKY_API_KEY="70573f8f45cd15ab543902b7d5540fcb"
 DARK_SKY_API_KEY = str(DARK_SKY_API_KEY).decode('utf8')
 
-response2 = requests.get('https://api.darksky.net/forecast/'+DARK_SKY_API_KEY+'/'+longitude+','+latitude)
+response = requests.get('https://api.darksky.net/forecast/'+DARK_SKY_API_KEY+'/'+longitude+','+latitude)
 
-json_data = json.loads(response2.text)
-print(json_data)
-print(response2)
+json_data = json.loads(response.text)
+print(json_data, response)
